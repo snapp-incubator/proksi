bb76bf9cc0d8fe296ff25f1533dc3e268855be20
Nima Mashhadi <nima.mashhadi@snapp.cab>
2022-11-17 00:42:20 +0330

    update servers

--- server/servers.go
+++ server/servers.go
@@ -1,20 +1,40 @@
-package server
+package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
 
+	"go.uber.org/zap"
+
 	"github.com/gorilla/mux"
 
 	"github.com/snapp-incubator/proksi/internal/logging"
 )
 
-// Server is HTTP server creator.
-func Server(address string, initRoutes func(r *mux.Router)) {
+func main() {
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	s1 := server("localhost:8080", initMainRoutes)
+	logging.L.Info("main server is running")
+	s2 := server("localhost:8081", initTestRoutes)
+	logging.L.Info("test server is running")
+
+	<-c
+	shutdown(s1)
+	shutdown(s2)
+	logging.L.Debug("all servers are down")
+}
+
+// server is HTTP server creator.
+func server(address string, initRoutes func(r *mux.Router)) *http.Server {
 	r := mux.NewRouter()
 
 	initRoutes(r)
@@ -34,14 +54,10 @@ func Server(address string, initRoutes func(r *mux.Router)) {
 		}
 	}()
 
-	c := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-	signal.Notify(c, os.Interrupt)
+	return srv
+}
 
-	// Block until we receive our signal.
-	<-c
-
+func shutdown(srv *http.Server) {
 	// Create a deadline to wait for.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(2)*time.Second)
 	defer cancel()
@@ -50,12 +66,97 @@ func Server(address string, initRoutes func(r *mux.Router)) {
 	// until the timeout deadline.
 	err := srv.Shutdown(ctx)
 	if err != nil {
-		logging.L.Debug(err)
+		logging.L.Debug("error in shutting down", zap.Error(err))
 	}
 
 	// Optionally, you could run srv.Shutdown in a goroutine and block on
 	// <-ctx.Done() if your application should wait for other services
 	// to finalize based on context cancellation.
 	logging.L.Debug("shutting down")
-	os.Exit(0)
+}
+
+func initMainRoutes(r *mux.Router) {
+	r.HandleFunc(
+		"/api/test",
+		mainServerHandler(),
+	).Methods("GET")
+}
+
+func initTestRoutes(r *mux.Router) {
+	r.HandleFunc(
+		"/api/test",
+		testServerHandler(),
+	).Methods("GET")
+}
+
+// handlers
+type requestBody struct {
+	Var1 string `json:"var1"`
+	Var2 int    `json:"var2"`
+	Var3 struct {
+		Var4 int    `json:"var4"`
+		Var5 string `json:"var5"`
+	} `json:"var3"`
+}
+
+type responseBody struct {
+	Var1 string `json:"var1"`
+	Var3 struct {
+		Var4 string `json:"var4"`
+		Var5 string `json:"var5"`
+	} `json:"var3"`
+}
+
+func mainServerHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+
+		var rq requestBody
+		err := json.NewDecoder(r.Body).Decode(&rq)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			logging.L.Warn("error in decode request body", zap.Error(err))
+			return
+		}
+
+		var res responseBody
+		res.Var1 = "success"
+		res.Var3.Var4 = "response from main server"
+		res.Var3.Var5 = "processed request"
+
+		err = json.NewEncoder(w).Encode(&res)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			logging.L.Warn("error in encode response body", zap.Error(err))
+			return
+		}
+	}
+}
+
+func testServerHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+
+		var rq requestBody
+		err := json.NewDecoder(r.Body).Decode(&rq)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			logging.L.Warn("error in decode request body", zap.Error(err))
+			return
+		}
+
+		var res responseBody
+		res.Var1 = "success"
+		res.Var3.Var4 = "response from test server"
+		res.Var3.Var5 = "processed request"
+
+		err = json.NewEncoder(w).Encode(&res)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			logging.L.Warn("error in encode response body", zap.Error(err))
+			return
+		}
+	}
 }
