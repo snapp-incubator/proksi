d4a4f8e30c34f4aff31d2a7e05034fa393233a17
Nima Mashhadi <nima.mashhadi@snapp.cab>
2022-11-17 00:45:58 +0330

    remove something

--- server/servers.go
+++ server/servers.go
@@ -18,8 +18,6 @@ import (
 
 func main() {
 	c := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
 	signal.Notify(c, os.Interrupt)
 
 	s1 := server("localhost:8080", initMainRoutes)
@@ -47,7 +45,6 @@ func server(address string, initRoutes func(r *mux.Router)) *http.Server {
 		Handler:      r,
 	}
 
-	// Run our server in a goroutine so that it doesn't block.
 	go func() {
 		if err := srv.ListenAndServe(); err != nil {
 			log.Fatal(err)
@@ -58,21 +55,13 @@ func server(address string, initRoutes func(r *mux.Router)) *http.Server {
 }
 
 func shutdown(srv *http.Server) {
-	// Create a deadline to wait for.
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(2)*time.Second)
 	defer cancel()
 
-	// Doesn't block if no connections, but will otherwise wait
-	// until the timeout deadline.
 	err := srv.Shutdown(ctx)
 	if err != nil {
 		logging.L.Debug("error in shutting down", zap.Error(err))
 	}
-
-	// Optionally, you could run srv.Shutdown in a goroutine and block on
-	// <-ctx.Done() if your application should wait for other services
-	// to finalize based on context cancellation.
-	logging.L.Debug("shutting down")
 }
 
 func initMainRoutes(r *mux.Router) {
